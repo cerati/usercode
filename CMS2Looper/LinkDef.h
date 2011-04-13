d4c514cd94f93bc9457a10cca709178af8ed0361
cerati <>
2011-04-13 09:10:42 +0000

    *** empty log message ***

---
+++ CMS2Looper/LinkDef.h
@@ -0,0 +1,11 @@
+#include "looper.h"
+
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class looper;
+//ScanChain(bool, std::string, TChain*, int, std::string);
+
+#endif
