08f9f1a49d77ed85413c750f59365b43d328c1d9
cerati <>
2011-11-21 10:19:46 +0000

    update for Full11, Summer11 MC

--- HWWScripts/fakeBg.C
+++ HWWScripts/fakeBg.C
@@ -2,35 +2,75 @@
 
 //please set useMit = false for FR
 
-pair<float, float> getSpillage(TString dir, unsigned int cut, unsigned int veto, int mass, int njets, TString region, float lumi, bool applyEff, bool doPUw) {
+pair<float, float> getSpillage(TString dir, unsigned int cut_nolep, unsigned int veto, int mass, int njets, TString region, float lumi, bool applyEff, bool doPUw) {
 
-  pair<float, float> qqwwFake_1  = getYield(dir+"qqww", cut|Lep2FullSelection, veto, mass, njets, region, lumi, false, applyEff, true, doPUw);
-  pair<float, float> qqwwFake_2  = getYield(dir+"qqww", cut|Lep1FullSelection, veto, mass, njets, region, lumi, false, applyEff, true, doPUw);
-
-  pair<float, float> ggwwFake_1  = getYield(dir+"ggww", cut|Lep2FullSelection, veto, mass, njets, region, lumi, false, applyEff, true, doPUw);
-  pair<float, float> ggwwFake_2  = getYield(dir+"ggww", cut|Lep1FullSelection, veto, mass, njets, region, lumi, false, applyEff, true, doPUw);
-
-  pair<float, float> ttbarFake_1 = getYield(dir+"ttbar",cut|Lep2FullSelection, veto, mass, njets, region, lumi, false, applyEff, true, doPUw);
-  pair<float, float> ttbarFake_2 = getYield(dir+"ttbar",cut|Lep1FullSelection, veto, mass, njets, region, lumi, false, applyEff, true, doPUw);
-
-  pair<float, float> twFake_1 = getYield(dir+"tw",cut|Lep2FullSelection, veto, mass, njets, region, lumi, false, applyEff, true, doPUw);
-  pair<float, float> twFake_2 = getYield(dir+"tw",cut|Lep1FullSelection, veto, mass, njets, region, lumi, false, applyEff, true, doPUw);
-
-  pair<float, float> wzFake_1 = getYield(dir+"wz",cut|Lep2FullSelection, veto, mass, njets, region, lumi, false, applyEff, true, doPUw);
-  pair<float, float> wzFake_2 = getYield(dir+"wz",cut|Lep1FullSelection, veto, mass, njets, region, lumi, false, applyEff, true, doPUw);
-
-  pair<float, float> zzFake_1 = getYield(dir+"zz_py",cut|Lep2FullSelection, veto, mass, njets, region, lumi, false, applyEff, true, doPUw);
-  pair<float, float> zzFake_2 = getYield(dir+"zz_py",cut|Lep1FullSelection, veto, mass, njets, region, lumi, false, applyEff, true, doPUw);
-
-  //cout << "qqww: " << qqwwFake_1.first+qqwwFake_2.first << " ggww: " << ggwwFake_1.first+ggwwFake_2.first << " ttbar: " << ttbarFake_1.first+ttbarFake_2.first 
-  //     << " tw: " << twFake_1.first+twFake_2.first << " wz: " << wzFake_1.first+wzFake_2.first << " zz: " << zzFake_1.first+zzFake_2.first << endl;
-
-  float spillYield = qqwwFake_1.first+ggwwFake_1.first+ttbarFake_1.first+twFake_1.first+wzFake_1.first+zzFake_1.first+
-    qqwwFake_2.first+ggwwFake_2.first+ttbarFake_2.first+twFake_2.first+wzFake_2.first+zzFake_2.first;
-  float spillError = sqrt(pow(qqwwFake_1.second,2)+pow(ggwwFake_1.second,2)+pow(ttbarFake_1.second,2)+pow(twFake_1.second,2)+pow(wzFake_1.second,2)+pow(zzFake_1.second,2)
-			  +pow(qqwwFake_2.second,2)+pow(ggwwFake_2.second,2)+pow(ttbarFake_2.second,2)+pow(twFake_2.second,2)+pow(wzFake_2.second,2)+pow(zzFake_2.second,2));
-
+  bool debug = 0;
+  region = region+=",spill";
+  //get scale factors for DY  from common.C
+  float dySF = 1.;
+  if (njets==0){
+    dySF=dysf0j;
+  } else if (njets==1) {
+    dySF=dysf1j;
+  }else if (njets==2) {
+    dySF=dysf2j;
+  }
+  //correct for spillage...
+  pair<float, float> qqwwFake    = getYield(main_dir+"wwSelNoLepNoTV/"+"qqww",  cut_nolep, veto, mass, njets, region, lumi, false, applyEff, true, doPUw);//FIXME
+  pair<float, float> ggwwFake    = getYield(dir+"ggww",  cut_nolep, veto, mass, njets, region, lumi, false, applyEff, true, doPUw);
+  pair<float, float> ttbarFake = getYield(dir+"ttbar", cut_nolep, veto, mass, njets, region, lumi, false, applyEff, true, doPUw);
+  pair<float, float> twFake    = getYield(dir+"tw", cut_nolep, veto, mass, njets, region, lumi, false, applyEff, true, doPUw);
+  pair<float, float> dymmFake  = getYield(dir+"dymm", cut_nolep, veto, mass, njets, region, lumi, false, applyEff, true, doPUw);
+  pair<float, float> dyeeFake  = getYield(dir+"dyee", cut_nolep, veto, mass, njets, region, lumi, false, applyEff, true, doPUw);
+  pair<float, float> wzFake    = getYield(dir+"wz", cut_nolep, veto, mass, njets, region, lumi, false, applyEff, true, doPUw);
+  pair<float, float> zzFake    = getYield(dir+"zz_py", cut_nolep, veto, mass, njets, region, lumi, false, applyEff, true, doPUw);
+  float spillYield = qqwwFake.first+ggwwFake.first+ttbarFake.first+twFake.first+wzFake.first+zzFake.first+dySF*dymmFake.first+dySF*dyeeFake.first;
+  float spillError = sqrt(pow(qqwwFake.second,2)+pow(ggwwFake.second,2)+pow(ttbarFake.second,2)+pow(twFake.second,2)+
+			 pow(wzFake.second,2)+pow(zzFake.second,2)+pow(dySF*dymmFake.second,2)+pow(dySF*dyeeFake.second,2));
+  if (debug) {
+    cout << "qqww: " << qqwwFake.first << " ggww: " << ggwwFake.first << " ttbar: " << ttbarFake.first << " tw: " << twFake.first 
+	 << " dymm: " << dySF*dymmFake.first << " dyee: " << dySF*dyeeFake.first << " wz: " << wzFake.first << " zz: " << zzFake.first << endl;
+  }
   return make_pair<float, float>(spillYield,spillError);
+}
+
+pair<float, float> fakeBgEstimation(TString dir, unsigned int cut, unsigned int veto, int mass, unsigned int njets=0, TString region="dphijet,minmet40", 
+				    float lumi=0., bool useJson=false, bool applyEff=true, bool doPUw=false)  {
+
+  //fixme integrate getSpillage
+  bool debug = 0;
+
+  unsigned int cut_nolep = cut&~Lep1FullSelection;
+  cut_nolep = cut_nolep&~Lep2FullSelection;
+
+  //get scale factors for DY  from common.C
+  float dySF = 1.;
+  if (njets==0){
+    dySF=dysf0j;
+  } else if (njets==1) {
+    dySF=dysf1j;
+  }else if (njets==2) {
+    dySF=dysf2j;
+  }
+
+  pair<float, float> dataFake  = getYield(dir+"data.root", cut_nolep, veto, mass, njets, region, 0, useJson, false, true, false);
+  //correct for spillage...
+  pair<float, float> spill=getSpillage(dir,  cut_nolep, veto, mass, njets, region, lumi, applyEff, doPUw);
+  float fakeYield = dataFake.first-spill.first;
+  float fakeError = sqrt(pow(dataFake.second,2)+pow(spill.second,2));
+  if (debug) {
+    cout << "total estimate, bare data, spill: " << fakeYield  << "+/-" << fakeError 
+	 << " " << dataFake.first << "+/-" << dataFake.second << " " << spill.first << "+/-" << spill.second<< endl;
+  }
+  return make_pair<float, float>(fakeYield,fakeError);
+}
+
+pair<float, float> fakeBgEstimationWithSyst(TString dir, unsigned int cut, unsigned int veto, int mass, unsigned int njets=0, TString region="dphijet,minmet40", 
+					    float lumi=0., bool useJson=false, bool applyEff=true, bool doPUw=false)  {
+
+  pair<float, float> fake=fakeBgEstimation(dir, cut,veto,mass,njets,region,lumi,useJson,applyEff,doPUw);
+  //assume 35% syst uncertainty
+  return make_pair<float, float>(fake.first,sqrt(pow(fake.first*0.35,2)+pow(fake.second,2)));
 }
 
 void makeFakeTable(float lumi) {
@@ -44,9 +84,9 @@ void makeFakeTable(float lumi) {
   bool doSpillage = 1;
 
   int mass = 0;
-  TString region = "dphireg,dphijet,minmetvtx,lep2pt15,ptll45";
+  TString region = "dphireg,dphijet,minmetvtx,lep2pt15,ptll45,mll20";
 
-  //int jetbins[] = {2};
+  //int jetbins[] = {0};
   int jetbins[] = {0,1,2};
   int njetbins = sizeof(jetbins)/sizeof(int);
 
@@ -54,87 +94,39 @@ void makeFakeTable(float lumi) {
 
     int njets = jetbins[j];
 
-    pair<float, float> wjMC   = getYield(main_dir+topww_dir+"wjets",  wwSelection, noVeto, mass, njets, region, lumi, useJson, applyTnPSF, false, true);
-    
-    //electron+fake electron
-    pair<float, float> wjData_ee1 = getYield(main_dir+topww_dir+"data.root", wwSelectionNoLep|Lep2FullSelection, noVeto, mass, njets, region+",eefs", 0, useJson, false, true, false);
-    pair<float, float> wjData_ee2 = getYield(main_dir+topww_dir+"data.root", wwSelectionNoLep|Lep1FullSelection, noVeto, mass, njets, region+",eefs", 0, useJson, false, true, false);
-    pair<float, float> spill_ee = make_pair<float, float>(0,0);
-    if (doSpillage) {
-      spill_ee = getSpillage(main_dir+topww_dir,wwSelectionNoLep, noVeto, mass, njets, region+",eefs", lumi, applyTnPSF, doPUw);
-    }
-    float ee = wjData_ee1.first+wjData_ee2.first-spill_ee.first;
-    float ee_err = sqrt(pow(wjData_ee1.second,2)+pow(wjData_ee2.second,2)+pow(spill_ee.second,2));
-    
-    //muon+fake electron
-    pair<float, float> wjData_me1 = getYield(main_dir+topww_dir+"data.root", wwSelectionNoLep|Lep2FullSelection, noVeto, mass, njets, region+",emfs", 0, useJson, false, true, false);
-    pair<float, float> wjData_me2 = getYield(main_dir+topww_dir+"data.root",  wwSelectionNoLep|Lep1FullSelection, noVeto, mass, njets, region+",mefs", 0, useJson, false, true, false);
-    pair<float, float> spill_me = make_pair<float, float>(0,0);
-    if (doSpillage) {
-      spill_me = getSpillage(main_dir+topww_dir,wwSelectionNoLep, noVeto, mass, njets, region+",mefs", lumi, applyTnPSF, doPUw);
-    }
-    float me = wjData_me1.first+wjData_me2.first-spill_me.first;
-    float me_err = sqrt(pow(wjData_me1.second,2)+pow(wjData_me2.second,2)+pow(spill_me.second,2));
-    
-    //electron+fake muon
-    pair<float, float> wjData_em1 = getYield(main_dir+topww_dir+"data.root", wwSelectionNoLep|Lep2FullSelection, noVeto, mass, njets, region+",mefs", 0, useJson, false, true, false);
-    pair<float, float> wjData_em2 = getYield(main_dir+topww_dir+"data.root",  wwSelectionNoLep|Lep1FullSelection, noVeto, mass, njets, region+",emfs", 0, useJson, false, true, false);
-    pair<float, float> spill_em = make_pair<float, float>(0,0);
-    if (doSpillage) {
-      spill_em = getSpillage(main_dir+topww_dir,wwSelectionNoLep, noVeto, mass, njets, region+",emfs", lumi, applyTnPSF, doPUw);
-    }
-    float em = wjData_em1.first+wjData_em2.first-spill_em.first;
-    float em_err = sqrt(pow(wjData_em1.second,2)+pow(wjData_em2.second,2)+pow(spill_em.second,2));
-    
-    //muon+fake muon
-    pair<float, float> wjData_mm1 = getYield(main_dir+topww_dir+"data.root", wwSelectionNoLep|Lep2FullSelection, noVeto, mass, njets, region+",mmfs", 0, useJson, false, true, false);
-    pair<float, float> wjData_mm2 = getYield(main_dir+topww_dir+"data.root", wwSelectionNoLep|Lep1FullSelection, noVeto, mass, njets, region+",mmfs", 0, useJson, false, true, false);
-    pair<float, float> spill_mm = make_pair<float, float>(0,0);
-    if (doSpillage) {
-      spill_mm = getSpillage(main_dir+topww_dir,wwSelectionNoLep, noVeto, mass, njets, region+",mmfs", lumi, applyTnPSF, doPUw);
-    }
-    float mm = wjData_mm1.first+wjData_mm2.first-spill_mm.first;
-    float mm_err = sqrt(pow(wjData_mm1.second,2)+pow(wjData_mm2.second,2)+pow(spill_mm.second,2));
-    
-    if (printAll) {      
-      cout << "bare data e + fake e: " << wjData_ee1.first+wjData_ee2.first << "+/-" << sqrt(pow(wjData_ee1.second,2)+pow(wjData_ee2.second,2)) << endl;
-      cout << "spill     e + fake e: " << spill_ee.first << "+/-" << spill_ee.second << endl;
-      cout << "tot data  e + fake e: " << ee << "+/-" << ee_err << endl;      
-      cout << "bare data m + fake e: " << wjData_me1.first+wjData_me2.first << "+/-" << sqrt(pow(wjData_me1.second,2)+pow(wjData_me2.second,2)) << endl;
-      cout << "spill     m + fake e: " << spill_me.first << "+/-" << spill_me.second << endl;
-      cout << "tot data  m + fake e: " << me << "+/-" << me_err << endl;      
-      cout << "bare data e + fake m: " << wjData_em1.first+wjData_em2.first << "+/-" << sqrt(pow(wjData_em1.second,2)+pow(wjData_em2.second,2)) << endl;
-      cout << "spill     e + fake m: " << spill_em.first << "+/-" << spill_em.second << endl;
-      cout << "tot data  e + fake m: " << em << "+/-" << em_err << endl;      
-      cout << "bare data m + fake m: " << wjData_mm1.first+wjData_mm2.first << "+/-" << sqrt(pow(wjData_mm1.second,2)+pow(wjData_mm2.second,2)) << endl;
-      cout << "spill     m + fake m: " << spill_mm.first << "+/-" << spill_mm.second << endl;
-      cout << "tot data  m + fake m: " << mm << "+/-" << mm_err << endl;      
-    }
-    
-    cout << "------------------- " << njets << "-jet bin -------------------------" << endl;
-    cout << Form("| %-24s | %-24s |",
-		 "Electron + Fake Electron",
-		 "Muon + Fake Electron") 
-	 << endl;
-    cout << Form("|     %6.2f +/- %-6.2f    |     %6.2f +/- %-6.2f    |",
-		 round(100.*ee)/100.,round(100.*ee_err)/100.,
-		 round(100.*me)/100.,round(100.*me_err)/100.) 
-	 << endl;
-    cout << "-------------------------------------------------------" << endl;
-    cout << Form("| %-24s | %-24s |",
-		 "Electron + Fake Muon",
-		 "Muon + Fake Muon")
-	 << endl;
-    cout << Form("|     %6.2f +/- %-6.2f    |     %6.2f +/- %-6.2f    |",
-		 round(100.*em)/100.,round(100.*em_err)/100.,
-		 round(100.*mm)/100.,round(100.*mm_err)/100.) 
-	 << endl;
-    cout << "-------------------------------------------------------" << endl;
-    cout << "tot Data: " << ee+me+em+mm << "+/-" << sqrt(pow(ee_err,2)+pow(me_err,2)+pow(em_err,2)+pow(mm_err,2)) << endl;
-    cout << "MC: " << wjMC.first << "+/-" << wjMC.second << endl;
-    cout << "-------------------------------------------------------" << endl;
-  }
+    pair<float, float> wjMC   = getYield(main_dir+topww_dir+"/wjets",  wwSelection, noVeto, mass, njets, region, lumi, useJson, applyTnPSF, false, doPUw);
+    //pair<float, float> wjMC_mm   = getYield(main_dir+topww_dir+"/wjets",  wwSelection, noVeto, mass, njets, region+"mmfs", lumi, useJson, applyTnPSF, false, doPUw);
+    //pair<float, float> wjMC_em   = getYield(main_dir+topww_dir+"/wjets",  wwSelection, noVeto, mass, njets, region+"emfs", lumi, useJson, applyTnPSF, false, doPUw);
+    //pair<float, float> wjMC_me   = getYield(main_dir+topww_dir+"/wjets",  wwSelection, noVeto, mass, njets, region+"mefs", lumi, useJson, applyTnPSF, false, doPUw);
+    //pair<float, float> wjMC_ee   = getYield(main_dir+topww_dir+"/wjets",  wwSelection, noVeto, mass, njets, region+"eefs", lumi, useJson, applyTnPSF, false, doPUw);
+    bool newversion = true;
+    bool doLatex = true;
 
+    pair<float, float> wjdatamm = fakeBgEstimation(main_dir+topww_dir,wwSelection, noVeto, mass, njets, region+"mmfs", lumi, useJson, applyTnPSF,doPUw);
+    pair<float, float> wjdatame = fakeBgEstimation(main_dir+topww_dir,wwSelection, noVeto, mass, njets, region+"mefs", lumi, useJson, applyTnPSF,doPUw);
+    pair<float, float> wjdataem = fakeBgEstimation(main_dir+topww_dir,wwSelection, noVeto, mass, njets, region+"emfs", lumi, useJson, applyTnPSF,doPUw);
+    pair<float, float> wjdataee = fakeBgEstimation(main_dir+topww_dir,wwSelection, noVeto, mass, njets, region+"eefs", lumi, useJson, applyTnPSF,doPUw);
+    if (!doLatex) {
+      cout << njets << "-jet bin" << endl;
+      cout << Form("mm: %5.1f +/- %5.1f - me: %5.1f +/- %5.1f - em: %5.1f +/- %5.1f - ee: %5.1f +/- %5.1f",
+		   wjdatamm.first,wjdatamm.second,
+		   wjdatame.first,wjdatame.second,
+		   wjdataem.first,wjdataem.second,
+		   wjdataee.first,wjdataee.second) << endl;
+      cout << Form("tot Data: %5.1f +/- %5.1f",wjdatamm.first+wjdatame.first+wjdataem.first+wjdataee.first,
+		   sqrt(pow(wjdatamm.second,2)+pow(wjdatame.second,2)+pow(wjdataem.second,2)+pow(wjdataee.second,2))) << endl;
+      cout << Form("MC: %5.1f +/- %5.1f", wjMC.first, wjMC.second) << endl;
+    }else {
+      cout << Form(" %i & %5.1f $\\pm$ %5.1f & %5.1f $\\pm$ %5.1f & %5.1f $\\pm$ %5.1f & %5.1f $\\pm$ %5.1f & %5.1f $\\pm$ %5.1f \\\\",
+		   njets,
+		   wjdatamm.first,wjdatamm.second,
+		   wjdatame.first,wjdatame.second,
+		   wjdataem.first,wjdataem.second,
+		   wjdataee.first,wjdataee.second,
+		   wjdatamm.first+wjdatame.first+wjdataem.first+wjdataee.first,sqrt(pow(wjdatamm.second,2)+pow(wjdatame.second,2)+pow(wjdataem.second,2)+pow(wjdataee.second,2))) 
+	   << endl;
+    }
+  }
 }
 
 void fakeBg(float lumi) {
