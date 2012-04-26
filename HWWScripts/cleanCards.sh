aa174e8d94ea9d582be99d9d7a7e2bfb67cd86f7
cerati <>
2012-04-26 12:31:09 +0000

    This commit was manufactured by cvs2svn to create tag 'V00-00-00'.
    Sprout from master 2012-04-26 12:31:08 UTC cerati 'first commit'
    Delete:
        AnalyzePAT/crabPAT_Ztautau.cfg
        AnalyzePAT/myRunZtoElecMu_cfg.py
        AnalyzePAT/rootlogon.C
        AnalyzePAT/testPatDiTaus.C
        AnalyzePAT/testPatElectrons.C
        AnalyzePAT/testPatJets.C
        AnalyzePAT/testPatMet.C
        AnalyzePAT/testPatMuons.C
        AnalyzePAT/testPatTrigger.C
        AnalyzePAT/testPatVertex.C
        CMS2Looper/LinkDef.h
        CMS2Looper/Makefile
        CMS2Looper/looper.cc
        CMS2Looper/looper.h
        CMS2Looper/main.cc
        HWWScripts/README.txt
        HWWScripts/cardMaker.C
        HWWScripts/cards/limits_0j_cut.txt
        HWWScripts/cards/limits_0j_shape.txt
        HWWScripts/cards/limits_0jof_cut.txt
        HWWScripts/cards/limits_0jof_shape.txt
        HWWScripts/cards/limits_0jsf_cut.txt
        HWWScripts/cards/limits_0jsf_shape.txt
        HWWScripts/cards/limits_1j_cut.txt
        HWWScripts/cards/limits_1j_shape.txt
        HWWScripts/cards/limits_1jof_cut.txt
        HWWScripts/cards/limits_1jof_shape.txt
        HWWScripts/cards/limits_1jsf_cut.txt
        HWWScripts/cards/limits_1jsf_shape.txt
        HWWScripts/cards/limits_2j_cut.txt
        HWWScripts/cards/limits_nj_cut.txt
        HWWScripts/cards/limits_nj_shape.txt
        HWWScripts/cards/limits_njof_cut.txt
        HWWScripts/cards/limits_njof_shape.txt
        HWWScripts/cards/limits_njsf_cut.txt
        HWWScripts/cards/limits_njsf_shape.txt
        HWWScripts/cleanCards.sh
        HWWScripts/common.C
        HWWScripts/dyBg.C
        HWWScripts/estimateBkg.sh
        HWWScripts/fakeBg.C
        HWWScripts/makeTable.C
        HWWScripts/plotBaby.C
        HWWScripts/runCardMaking.sh
        HWWScripts/shapeMaker.C
        HWWScripts/shapeMaker.py
        HWWScripts/skim.C
        HWWScripts/tdrStyle.C
        HWWScripts/topBg.C
        HWWScripts/wwBkg.C
        HWWScripts/zzBg.C
        LowPtEid/Makefile
        LowPtEid/Makefile.arch
        LowPtEid/ScanChain.C
        LowPtEid/doData.C
        LowPtEid/doGPt30to50.C
        LowPtEid/doMC.C
        LowPtEid/doWToENu.C
        LowPtEid/makeFOMSIdOnlyHWW130.C
        LowPtEid/makeFOMSIpOnlyHWW130.C
        LowPtEid/makeFOMSIsoOnlyHWW130.C
        LowPtEid/makePlotIdOnlyHWW130.C
        LowPtEid/makePlotIpOnlyHWW130.C
        LowPtEid/makePlotIsoOnlyHWW130.C
        LowPtEid/myBabyMaker.C
        LowPtEid/myBabyMaker.h
        ZAnalysis7TeV/BuildFile
        ZAnalysis7TeV/bsubMinBias.sh
        ZAnalysis7TeV/bsubReReco.sh
        ZAnalysis7TeV/crabGoodColl.cfg
        ZAnalysis7TeV/crabMinBias.cfg
        ZAnalysis7TeV/crabReReco.cfg
        ZAnalysis7TeV/ntuplizer_zanalysis7tev_PAT_MinBias_cfg.py
        ZAnalysis7TeV/ntuplizer_zanalysis7tev_PAT_ReReco_cfg.py
        ZAnalysis7TeV/ntuplizer_zanalysis7tev_PAT_cfg.py
        ZAnalysis7TeV/skimElecMu_MinBias_cfg.py
        ZAnalysis7TeV/skimElecMu_ReReco_cfg.py
        ZAnalysis7TeV/skimElecMu_cfg.py
        ZAnalysis7TeV/src/NtuplizerZAnalysis7TeVPAT.cc
        ZAnalysis7TeV/test2.C

--- HWWScripts/cleanCards.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm cards/*/*txt_*
-rm cards/*.gif
-rm cards/*.pdf
-rm -rf cards/output
-rm -rf cards/log
-rm /tmp/tmp.*
--- HWWScripts/runCardMaking.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash                                                                                                                                                                  
-
-root -b -q -l cardMaker.C++\(4.7,\"cut\"\)
-root -b -q -l cardMaker.C++\(4.7,\"shape\"\)
-./shapeMaker.py 4.7
