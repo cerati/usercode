9130289065002582c89bfe4ef48ef83450d20743
cerati <>
2013-03-22 09:36:52 +0000

    update

--- HWWScripts/SubmitToys/gcRunNormTest.sh
+++ HWWScripts/SubmitToys/gcRunNormTest.sh
@@ -2,10 +2,10 @@
 
 # this code submits the bias studies to the lsf queue
 #--------Test example, submitting 1 job to the queue
-# ./gcRunNormTest.sh 125 0 0 hww
+# ./gcRunNormTest.sh 125 0 0 hww cards_inj_stat
 #--------Realistic example, submit 10 jobs into the queue, which 
 #     analyses 100 toys 
-# for j in {0,100,200,300,400,500,600,700,800,900}; do ./gcRunNormTest.sh 125 $(($j+1)) $(($j+100)) hww; done
+# for j in {0,100,200,300,400,500,600,700,800,900}; do ./gcRunNormTest.sh 125 $(($j+1)) $(($j+100)) hww cards_inj_stat; done
 
 
 #this is def or 125
@@ -42,32 +42,34 @@ MIN=\$2
 MAX=\$3
 MASS=\$4
 cd /tmp/cerati/
-scramv1 p CMSSW CMSSW_5_3_3
-cd CMSSW_5_3_3/src
+export SCRAM_ARCH=slc5_amd64_gcc472
+scramv1 p CMSSW CMSSW_6_1_1
+cd CMSSW_6_1_1/src
 eval \`scramv1 ru -sh\`
-addpkg HiggsAnalysis/CombinedLimit V02-02-03 
-scramv1 b
+addpkg HiggsAnalysis/CombinedLimit V03-01-00 
+scramv1 b -j 4
 perl -p -i -e "s/exp\_/exp\_final\_/g" HiggsAnalysis/CombinedLimit/test/mlfitNormsToText.py
+cp $WORKDIR/diffNuisances.py HiggsAnalysis/CombinedLimit/test/
 cp $WORKDIR/getPseudoData.C .
 root -b -q getPseudoData.C\(\"$INPUTDIR\",\${MASS},\"8TeV\",\"shape\",0,\"of\",\"${ANA}\",\${MIN},\${MAX}\)
 root -b -q getPseudoData.C\(\"$INPUTDIR\",\${MASS},\"8TeV\",\"shape\",1,\"of\",\"${ANA}\",\${MIN},\${MAX}\)
-echo 'processing mass='\${MASS} analysis ${ANA} $NJET jet>> /tmp/cerati/CMSSW_5_3_3/src/log_\${INJ}_\${MASS}_\${MIN}_\${MAX}_$ANA_summary.log
+echo 'processing mass='\${MASS} analysis ${ANA} $NJET jet>> /tmp/cerati/CMSSW_6_1_1/src/log_\${INJ}_\${MASS}_\${MIN}_\${MAX}_$ANA_summary.log
 for NEXP in \$(seq \${MIN} \${MAX})
   do
   echo 'toy='\${NEXP} 
   DIRPATH=testcards/cards_\${INJ}_N\${NEXP}/\${MASS}/
   cd \${DIRPATH}
   combine -M MaxLikelihoodFit --saveNorm ${ANA}of_0j_shape_8TeV.txt -m \${MASS} #>& /dev/null
-  python ../../../HiggsAnalysis/CombinedLimit/test/mlfitNormsToText.py mlfit.root >> /tmp/cerati/CMSSW_5_3_3/src/logNorm_\${INJ}_\${MASS}_${ANA}of_0j_shape_8TeV_\${MIN}_\${MAX}.log
-  python ../../../HiggsAnalysis/CombinedLimit/test/diffNuisances.py -a mlfit.root >> /tmp/cerati/CMSSW_5_3_3/src/logNuis_\${INJ}_\${MASS}_${ANA}of_0j_shape_8TeV_\${MIN}_\${MAX}.log
-  cp mlfit.root /tmp/cerati/CMSSW_5_3_3/src/mlfit_injm${INJ}_m\${MASS}_${ANA}of_0j_id\${NEXP}.root 
+  python ../../../HiggsAnalysis/CombinedLimit/test/mlfitNormsToText.py mlfit.root >> /tmp/cerati/CMSSW_6_1_1/src/logNorm_\${INJ}_\${MASS}_${ANA}of_0j_shape_8TeV_\${MIN}_\${MAX}.log
+  python ../../../HiggsAnalysis/CombinedLimit/test/diffNuisances.py -a mlfit.root >> /tmp/cerati/CMSSW_6_1_1/src/logNuis_\${INJ}_\${MASS}_${ANA}of_0j_shape_8TeV_\${MIN}_\${MAX}.log
+  cp mlfit.root /tmp/cerati/CMSSW_6_1_1/src/mlfit_injm${INJ}_m\${MASS}_${ANA}of_0j_id\${NEXP}.root 
   rm mlfit.root higgsCombineTest.MaxLikelihoodFit.mH125.root
   combine -M MaxLikelihoodFit --saveNorm ${ANA}of_1j_shape_8TeV.txt -m \${MASS} #>& /dev/null
-  python ../../../HiggsAnalysis/CombinedLimit/test/mlfitNormsToText.py mlfit.root >> /tmp/cerati/CMSSW_5_3_3/src/logNorm_\${INJ}_\${MASS}_${ANA}of_1j_shape_8TeV_\${MIN}_\${MAX}.log
-  python ../../../HiggsAnalysis/CombinedLimit/test/diffNuisances.py -a mlfit.root >> /tmp/cerati/CMSSW_5_3_3/src/logNuis_\${INJ}_\${MASS}_${ANA}of_1j_shape_8TeV_\${MIN}_\${MAX}.log
-  cp mlfit.root /tmp/cerati/CMSSW_5_3_3/src/mlfit_injm${INJ}_m\${MASS}_${ANA}of_1j_id\${NEXP}.root 
+  python ../../../HiggsAnalysis/CombinedLimit/test/mlfitNormsToText.py mlfit.root >> /tmp/cerati/CMSSW_6_1_1/src/logNorm_\${INJ}_\${MASS}_${ANA}of_1j_shape_8TeV_\${MIN}_\${MAX}.log
+  python ../../../HiggsAnalysis/CombinedLimit/test/diffNuisances.py -a mlfit.root >> /tmp/cerati/CMSSW_6_1_1/src/logNuis_\${INJ}_\${MASS}_${ANA}of_1j_shape_8TeV_\${MIN}_\${MAX}.log
+  cp mlfit.root /tmp/cerati/CMSSW_6_1_1/src/mlfit_injm${INJ}_m\${MASS}_${ANA}of_1j_id\${NEXP}.root 
   rm mlfit.root higgsCombineTest.MaxLikelihoodFit.mH125.root
-  cd /tmp/cerati/CMSSW_5_3_3/src
+  cd /tmp/cerati/CMSSW_6_1_1/src
 done
 mkdir -p $OUTPUTDIR/logsNorm/\${MASS}/
 # clear up old files
@@ -77,10 +79,10 @@ rm -f $OUTPUTDIR/logsNorm/\${MASS}/log_\${INJ}_\${MASS}_\${MIN}_\${MAX}_$ANA_0j_
 rm -f $OUTPUTDIR/logsNorm/\${MASS}/logNorm_\${INJ}_\${MASS}_${ANA}of_1j_shape_8TeV_\${MIN}_\${MAX}.log
 rm -f $OUTPUTDIR/logsNorm/\${MASS}/logNuis_\${INJ}_\${MASS}_${ANA}of_1j_shape_8TeV_\${MIN}_\${MAX}.log
 rm -f $OUTPUTDIR/logsNorm/\${MASS}/log_\${INJ}_\${MASS}_\${MIN}_\${MAX}_$ANA_1j_summary.log
-cp /tmp/cerati/CMSSW_5_3_3/src/log* $OUTPUTDIR/logsNorm/\${MASS}/
-cp /tmp/cerati/CMSSW_5_3_3/src/mlfit_* $OUTPUTDIR/logsNorm/\${MASS}/
+cp /tmp/cerati/CMSSW_6_1_1/src/log* $OUTPUTDIR/logsNorm/\${MASS}/
+cp /tmp/cerati/CMSSW_6_1_1/src/mlfit_* $OUTPUTDIR/logsNorm/\${MASS}/
 cd /tmp/cerati/
-rm -rf CMSSW_5_3_3
+rm -rf CMSSW_6_1_1
 EOF
 
 # submit the jobs
